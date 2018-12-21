5e87c3812f1cb86373b9fab9a7cdf2fb26a3ba69
Casey Ross <caseyross003@gmail.com>
2018-12-21 08:37:55 -0600

    add author to post and limit to ten posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,13 +14,23 @@ class Blog extends Component {
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts =response.data.slice(0, 10);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Casey'
+                    }
+                });
+                this.setState({posts: updatedPosts});
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} />
+            return <Post 
+                    key={post.id} 
+                    title={post.title}
+                    author={post.author} />
         });
 
         return (
