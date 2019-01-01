676d33ce7a715f1863d620b2e8fbcee90662266d
Casey Ross <caseyross003@gmail.com>
2019-01-01 14:43:09 -0600

    add NavLink to apply styling to active page link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -12,12 +12,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                    to="/" 
+                                    exact 
+                                    activeClassName="main-active"
+                                    activeStyle={{
+                                        fontWeight: 'bold'
+                                    }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
