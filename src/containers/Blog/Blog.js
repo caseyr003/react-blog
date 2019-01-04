2c8cc622c8f730e4f9b917ac9ffd531885527319
Casey Ross <caseyross003@gmail.com>
2019-01-03 20:00:46 -0600

    add 404 page to replace redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 class Blog extends Component {
     state = {
@@ -33,7 +33,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/posts" component={Posts} />
                     {this.state.auth && <Route path="/new-post" exact component={NewPost} />}
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>404 Error</h1>} />
                 </Switch>
             </div>
         );
